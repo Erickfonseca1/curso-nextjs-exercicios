9d4207159a5aaa2685a4a33f3e1427fb29997629
Erick Fonseca <erickboyfla@gmail.com>
2022-12-13 12:02:44 -0300

    exemplos iniciais de uso e configuração de jsx e desafio de criação de listas

---
+++ pages/basicos/jsx2.jsx
@@ -0,0 +1,10 @@
+export default function jsx2() {
+  //armazenamento de html em const
+  const conteudo = (
+    <div>
+      <h1>JSX #02 alterado</h1>
+    </div>
+  )
+
+  return conteudo
+}
---
+++ pages/basicos/jsx4.jsx
@@ -0,0 +1,21 @@
+export default function jsx4() {
+  const subtitulo = "Estou no JavaScript"
+  //uso de {} para interpretação do javaScript entro do html
+  return (
+    <div>
+      <h1>Integração JS e JSX</h1>
+      <h2>{subtitulo}</h2>
+      <h3>{3*3}</h3>
+      <h4>{Math.max(13,39)}</h4>
+      <h4>{entre(9, 1, 10)}</h4>
+    </div>
+  )
+}
+
+function entre(valor, min, max) {
+  if(valor >= min && valor <= max) {
+    return "sim"
+  } else {
+    return "não"
+  }
+}
